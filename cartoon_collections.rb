7c11f9c5baa59e323d0e6f3aa4f9f9822130cad9
Joseph Corona <josed.hc96@gmail.com>
2018-11-14 23:12:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,11 @@ def summon_captain_planet(elements)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? do |word|
+    return word.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
